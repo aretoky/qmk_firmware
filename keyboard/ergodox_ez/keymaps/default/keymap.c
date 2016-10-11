ce9cd9f45711e3035995be1c4bfd30042cd55c95
Yuki Kotera <kotera@radicode.co.jp>
2016-10-11 19:40:09 +0900

    fix

--- keyboard/ergodox_ez/keymaps/default/keymap.c
+++ keyboard/ergodox_ez/keymaps/default/keymap.c
@@ -24,19 +24,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |    1   |   2  |   3  |   4  |   5  |   6  |Grv/L2|           |   ^  |   7  |   8  |   9  |   0  |   -  |  \/L2  |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Tab    |   Q  |   W  |   E  |   R  |   T  |      |           |      |   Y  |   U  |   I  |   O  |   P  |   @    |
+ * | Tab    |   Q  |   W  |   E  |   R  |   T  |  BS  |           |   [  |   Y  |   U  |   I  |   O  |   P  |  @/CTL |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | LCtrl  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   :    |
- * |--------+------+------+------+------+------| Lclk |           | Rclk |------+------+------+------+------+--------|
- * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl| ¥ _ / S|
+ * |--------+------+------+------+------+------| Lclk |           |   ]  |------+------+------+------+------+--------|
+ * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | ~L1  |AltShf| LAlt | Cmd  | EISU |                                       | KANA |[/Cmd |]/RAlt|   ^  | ~L1  |
+ *   | ~L1  |AltShf| LAlt | Cmd  | EISU |                                       | KANA |  Cmd | RAlt |   _  | ~L1  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                  ,-------------------.       ,--------------.
  *                                  |   Cut   |  Paste  |       | LEFT | RIGHT |
  *                           ,------|---------|---------|       |------+-------+-------.
  *                           |      |         |  Copy   |       |  UP  |       |       |
- *                           | Space|Backspace|---------|       |------|BKSpace| Enter |
+ *                           | Space|Backspace|---------|       |------|  L1   | Enter |
  *
  *                           |      |         |  Undo   |       | DOWN |       |       |
  *                            `-------------------------'       `----------------------'
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
         KC_1,             KC_2,            KC_3,     KC_4,   KC_5,   KC_6,   LT(MDIA, KC_GRV), 
-        KC_TAB,           KC_Q,            KC_W,     KC_E,   KC_R,   KC_T,   MO(SYMB),
+        KC_TAB,           KC_Q,            KC_W,     KC_E,   KC_R,   KC_T,   KC_BSPC,
         KC_LCTRL,         KC_A,            KC_S,     KC_D,   KC_F,   KC_G,
         KC_LSFT,          KC_Z,            KC_X,     KC_C,   KC_V,   KC_B,   KC_BTN1,
         MO(SYMB),         LALT(KC_LSFT),   KC_LALT,  KC_LGUI,KC_LANG2,
@@ -55,13 +55,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                          KC_SPC,KC_BSPC,LGUI(KC_Z),
         // right hand
         JA_HAT,      KC_7,   KC_8,         KC_9,   KC_0,   KC_MINS,          LT(MDIA, JA_ENVL),
-        MO(SYMB),    KC_Y,   KC_U,         KC_I,   KC_O,   KC_P,             JA_AT,
+        JA_LBRC,    KC_Y,   KC_U,         KC_I,   KC_O,   KC_P,             CTL_T(JA_AT),
                      KC_H,   KC_J,         KC_K,   KC_L,   KC_SCLN,          JA_CLON,
-        KC_BTN2,     KC_N,   KC_M,         KC_COMM,KC_DOT, CTL_T(KC_SLSH),   SFT_T(JA_ENUN),
-                             KC_LANG1, GUI_T(JA_LBRC),ALT_T(JA_RBRC),JA_RBRC,       MO(SYMB),
+        JA_RBRC,     KC_N,   KC_M,         KC_COMM,KC_DOT, KC_SLSH,   KC_LSFT,
+                             KC_LANG1, KC_RGUI,KC_RALT,JA_ENUN,       MO(SYMB),
         KC_LEFT,     KC_RIGHT,
         KC_UP,
-        KC_DOWN,KC_BSPC,KC_ENT
+        KC_DOWN,MO(SYMB),KC_ENT
     ),
 /* Keymap 1: Symbol Layer
  *
@@ -108,7 +108,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 2: Media and mouse keys
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |   F1    |  F2  |  F3  |  F4  |  F5  |  F6  |      |           |  F7  |  F8  |  F9  | F10  | F11  | F12  |        |
+ * |   F1    |  F2  |  F3  |  F4  |  F5  |  F6  |RESET|           |  F7  |  F8  |  F9  | F10  | F11  | F12  |        |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -128,7 +128,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 // MEDIA AND MOUSE
 [MDIA] = KEYMAP(
-       KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_TRNS, 
+       KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   RESET, 
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
